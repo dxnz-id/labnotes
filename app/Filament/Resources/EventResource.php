072ed2957f9e070840b83691e7945921d6cc475b
dxnz-id <dxnzid@gmail.com>
2025-03-09 15:21:38 +0700

    view: calendar icon

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -36,7 +36,7 @@ class EventResource extends Resource
 {
     protected static ?string $model = Event::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
 
     /**
      * Updates the upload directories for all file upload fields when event name or date changes

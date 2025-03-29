7dac4c18d7917ff0d5d338874b90468c32868685
dxnz-id <dxnzid@gmail.com>
2025-03-29 16:10:19 +0700

    fix: blank

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,11 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements HasAvatar
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
